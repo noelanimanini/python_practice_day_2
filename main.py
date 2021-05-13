4e4015e814e61a82ce98a5bffaab14ef06caf1cc
crystal <noelani.villanueva@gmail.com>
2021-05-13 14:47:28 -0700

    web scraping pulling github profile image

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import turtle
-import another_module
+import requests
+from bs4 import BeautifulSoup as bs
 
-print(another_module.another_variable)
+github_user = input('Input GitHub User: ')
+url = 'https://github.com/' + github_user
+r = requests.get(url)
+soup = bs(r.content, 'html.parser') 
+profile_image = soup.find('img', {'alt' : 'Avatar'})['src']
+print(profile_image)
+
+
+
+
