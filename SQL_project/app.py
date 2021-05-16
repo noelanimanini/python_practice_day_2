31045cefb7ba402523f4f0f1537cdd9fad97532d
crystal <noelani.villanueva@gmail.com>
2021-05-15 18:05:13 -0700

    SQL and python practice

---
+++ SQL_project/app.py
@@ -0,0 +1,13 @@
+import main
+
+main.add_one("sarrah", "sebastian", "freddies@gmail.com")
+
+# main.delete_one('6')
+stuff = [
+  ("Brenda","Smitherton","Brenda@gmail.com"),
+  ("Josh","EW","josh@gmail.com")]
+main.add_many(stuff)
+
+main.email_lookup("smal@gmail.com")
+
+# main.show_all()
--- SQL_project/main.py
+++ SQL_project/main.py
@@ -1,50 +1,120 @@
-import sqlite3 
+import sqlite3
 
-conn = sqlite3.connect("customer.db")
+# many_customers = [('West', 'Brown', 'WB@gmail.com'), ('Jennie', 'Craig', 'JC@craig.com'), ('Dan', 'theGuy', 'dan@geop.com'),]
+# c.execute("""CREATE TABLE demo (
+#     first_name text, 
+#     last_name text, 
+#     description text
+# )
+# """)
 
-# create a cursor
-cursor = conn.cursor()
+def show_all():
+  conn = sqlite3.connect("demo.db")
+  c = conn.cursor()
+  c.execute("SELECT rowid, * FROM demo")
+
+  items = c.fetchall()
+  for item in items: 
+    print(item)
+  conn.commit()
+  conn.close()
+
+def add_one(first, last, email):
+  conn = sqlite3.connect("demo.db")
+  c = conn.cursor()
+  c.execute("INSERT INTO demo VALUES (?,?,?)", (first, last, email))
+  conn.commit()
+  conn.close()
+
+def add_many(list):
+  conn = sqlite3.connect("demo.db")
+  c = conn.cursor()
+  c.executemany("INSERT INTO demo VALUES (?,?,?)", (list))
+  conn.commit()
+  conn.close()
+
+def delete_one(id):
+  conn = sqlite3.connect('demo.db')
+  c = conn.cursor()
+  c.execute("DELETE from demo WHERE rowid=(?)", id)
+  conn.commit()
+  conn.close()
+
+def email_lookup(description):
+  conn = sqlite3.connect("demo.db")
+  c = conn.cursor()
+  c.execute("SELECT * from demo WHERE description=(?)", (description,))
+  items = c.fetchall()
+  for item in items: 
+    print(item)
+  conn.commit()
+  conn.close()
+# c.execute("""INSERT INTO demo VALUES (
+#   'crystal', 
+#   'smal',
+#   'smal@gmail.com'
+# )
+
+# """)
+
+
+# import sqlite3 
+
+# conn = sqlite3.connect("customer.db")
+
+# # create a cursor
+# cursor = conn.cursor()
 
 # many_customers = [('West', 'Brown', 'WB@gmail.com'), ('Jennie', 'Craig', 'JC@craig.com'), ('Dan', 'theGuy', 'dan@geop.com'),]
 
-# 1. create a table 
+# # 1. create a table 
 # cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+# # print("this works!")
 
-# print("this works!")
+# # example 1
+# # cursor.execute("""CREATE TABLE customers (
+# #   first_name text, 
+# #   last_name text, 
+# #   email text
+# # )""")
 
-# example 1
-# cursor.execute("""CREATE TABLE customers (
-#   first_name text, 
-#   last_name text, 
-#   email text
-# )""")
+# # cursor.execute("CREATE_TABLE customers (first_name DATATYPE, last_name DATATYPE, email DATATYPE)")
+# # ^unprefered syntax
 
-# cursor.execute("CREATE_TABLE customers (first_name DATATYPE, last_name DATATYPE, email DATATYPE)")
-# ^unprefered syntax
+# # SQLite3 datatypes: 
+#   # null (does is exist?), integer (num), real (num with decimal), text (string), blob (stored as it is)
 
-# SQLite3 datatypes: 
-  # null (does is exist?), integer (num), real (num with decimal), text (string), blob (stored as it is)
+# # 2. querying the database 
+# # fetchone(), fetchmany(), fetchall()
 
-# 2. querying the database 
-# fetchone(), fetchmany(), fetchall()
+# # updating records
+# # cursor.execute(""" 
+# #     UPDATE customers SET first_name = 'joahnna' 
+# #     WHERE rowid = 4
+# # """)
 
-# updating records
-cursor.execute(""" 
-    UPDATE customers set first_name = 'billy' 
-    WHERE rowid = 1
-""")
+# # Deleting records
+# # cursor.execute(""" 
+# #     DELETE from customers WHERE rowid = 6
+# # """)
+# # cursor.execute("DROP TABLE customer")
 
-conn.commit() 
-cursor.execute("SELECT rowid, * FROM customers") 
+# # drop table 
 
-# print(cursor.fetchall())
-items = cursor.fetchall()
-
-for item in items: 
-  print(item)
-
-# commit our command 
 # conn.commit()
 
-# close the connection 
-conn.close()
+# # conn.commit() 
+# # cursor.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC LIMIT 2") 
+
+
+# # print(cursor.fetchall())
+# items = cursor.fetchall()
+
+# for item in items: 
+#   print(items)
+
+# # commit our command 
+# # conn.commit()
+
+# # close the connection 
+# conn.close()
