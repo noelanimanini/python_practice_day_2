6ca7b21de83de4af116fbd229ca50dd405db7952
crystal <noelani.villanueva@gmail.com>
2021-05-17 18:11:35 -0700

    python practice

---
+++ inter_python/lists.py
@@ -0,0 +1,30 @@
+# mylist = ["cherries", "blueberries", "strawberries"]
+# print(mylist)
+
+# item = mylist[-1]
+# print(item)
+
+# for i in mylist:
+#   if i == "cherries":
+#     print("yes")
+#   else: 
+#     print("no")
+
+# item = len(mylist)
+# print(item)
+
+# mylist.append("lemon")
+# mylist.insert(1, "apple")
+# item = mylist.pop()
+
+# item = mylist.remove("blueberries")
+# mylist.clear()
+# mylist.reverse()
+# mylist.sort()
+# sorted() vs sort()
+# one makes a new array and one just sorts
+
+# slicing
+# mylist = [1,2,3,4,5,6]
+# a = mylist[1:5]
+# print(a)
