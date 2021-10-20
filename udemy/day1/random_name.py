fc33b7355e04616d5a0def45b944a91b5d54f3e3
crystal <noelani.villanueva@gmail.com>
2021-10-19 20:32:13 -0700

    random generator of names in the CLI

---
+++ udemy/day1/random_name.py
@@ -0,0 +1,18 @@
+# You are going to write a program which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+
+# Important: You are not allowed to use the choice() function.
+
+# Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as name followed by comma then space. e.g. name, name, name
+
+
+import random 
+
+names_string = input("what are the names.  ")
+
+names = names_string.split(",")
+
+print(names)
+
+answer = random.randint(0, len(names))
+
+print(f"{names[answer]} is paying for the bill ")
